e01bd88ee678a088870bcdc1ab11783723461e30
Andrea Venturi <andrea.venturi@cern.ch>
2014-01-05 23:24:02 +0100

    Added protection if data file does not exist

--- webpages/chart_functions.php
+++ webpages/chart_functions.php
@@ -32,24 +32,26 @@ for($i=$npastdays;$i>=0;$i--) {
    $daystring=getdate($then);
    foreach($logfiletemplates as $logfiletemplate) {
       $inputfile=sprintf($logfiletemplate,$daystring['year'],$daystring['mon'],$daystring['mday']);
-      $readings = file($inputfile);
-      foreach($readings as $line) {
-         $temp_array=explode(" ",$line);
-         $date=explode("-",$temp_array[$dateindex[$logfiletemplate]]);
-         $year=$date[0];
-         $month=$date[1]-1;
-         $day=$date[2];
-         $time=explode(":",$temp_array[$timeindex[$logfiletemplate]]);
-         $address=$temp_array[$addressindex[$logfiletemplate]];
-         $measurement=$temp_array[$tempindex[$logfiletemplate]];
-         if(array_search($address,$addresses,true)!==false) {
-           echo "[new Date($year,$month,$day,$time[0],$time[1],0)";
-           foreach($addresses as $theaddress) {
-             if($address==$theaddress) { $corrmeas=$measurement+$offset[$theaddress]; echo ",$corrmeas"; }
-             else { echo ",null"; }
+      if(file_exists($inputfile)) {
+        $readings = file($inputfile);
+        foreach($readings as $line) {
+           $temp_array=explode(" ",$line);
+           $date=explode("-",$temp_array[$dateindex[$logfiletemplate]]);
+           $year=$date[0]; 
+           $month=$date[1]-1;
+           $day=$date[2];
+           $time=explode(":",$temp_array[$timeindex[$logfiletemplate]]);
+           $address=$temp_array[$addressindex[$logfiletemplate]];
+           $measurement=$temp_array[$tempindex[$logfiletemplate]];
+           if(array_search($address,$addresses,true)!==false) {
+             echo "[new Date($year,$month,$day,$time[0],$time[1],0)";
+             foreach($addresses as $theaddress) {
+               if($address==$theaddress) { $corrmeas=$measurement+$offset[$theaddress]; echo ",$corrmeas"; }
+               else { echo ",null"; }
+             }
+             echo "],";
            }
-           echo "],";
-         }
+        }
       }
    }
 }
