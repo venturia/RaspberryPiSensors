04ed83b42593a301fee6d3c328b89a6b912f032c
Andrea Venturi <andrea.venturi@cern.ch>
2014-01-23 22:43:52 +0100

    DS1621 with address 0x4b moved to rpi03

--- webpages/temperature_rpi03_params.php
+++ webpages/temperature_rpi03_params.php
@@ -1,6 +1,6 @@
 <?php
 $suffix="temperature";
-$logfiletemplates=["/home/pi/log_termometro_%04d-%02d-%02d.out"];
+$logfiletemplates=["/home/pi/log_termometro_%04d-%02d-%02d.out","/home/pi/log_termometro_%04d-%02d-%02d.out"];
 $dateindex=array();
 $dateindex["/home/pi/log_termometro_%04d-%02d-%02d.out"]=0;
 $timeindex=array();
@@ -9,8 +9,9 @@ $addressindex=array();
 $addressindex["/home/pi/log_termometro_%04d-%02d-%02d.out"]=6;
 $offset=array();
 $offset["0x4d"]=0;
-$addresses=["0x4d"];
-$labels=["Test new DS1621 (C)"];
+$offset["0x4b"]=0;
+$addresses=["0x4d","0x4b"];
+$labels=["Test new DS1621 (C)","Test DS1621 (C)"];
 $tempindex=array();
 $tempindex["/home/pi/log_termometro_%04d-%02d-%02d.out"]=11;
 $plottitle="'Temperatura'";
