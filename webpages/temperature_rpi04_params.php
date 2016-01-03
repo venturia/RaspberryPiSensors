085885ef3b97686bb796eebe4b14b22954b32dcb
Andrea Venturi <andrea.venturi@cern.ch>
2016-01-03 11:37:26 +0100

    Update of the temperature sensors of rpi04

--- webpages/temperature_rpi04_params.php
+++ webpages/temperature_rpi04_params.php
@@ -1,6 +1,6 @@
 <?php
 $suffix="temperature";
-$logfiletemplates=["/home/pi/log_DS18b20_%04d-%02d-%02d.out","/home/pi/log_DS18b20_%04d-%02d-%02d.out"];
+$logfiletemplates=["/home/pi/log_DS18b20_%04d-%02d-%02d.out","/home/pi/log_DS18b20_%04d-%02d-%02d.out","/home/pi/log_DS18b20_%04d-%02d-%02d.out"];
 $dateindex=array();
 $dateindex["/home/pi/log_DS18b20_%04d-%02d-%02d.out"]=0;
 $timeindex=array();
@@ -8,10 +8,11 @@ $timeindex["/home/pi/log_DS18b20_%04d-%02d-%02d.out"]=1;
 $addressindex=array();
 $addressindex["/home/pi/log_DS18b20_%04d-%02d-%02d.out"]=2;
 $offset=array();
-$offset["28-000005380077"]=0;
 $offset["28-000005959090"]=0;
-$addresses=["28-000005380077","28-000005959090"];
-$labels=["DS18b20 waterproof (C)","Ingresso (C)"];
+$offset["28-0000059553da"]=0;
+$offset["28-00000595887c"]=0;
+$addresses=["28-000005959090","28-0000059553da","28-00000595887c"];
+$labels=["Ingresso (C)","DS18B20 test 1 (C)","DS18B20 test 2 (C)"];
 $tempindex=array();
 $tempindex["/home/pi/log_DS18b20_%04d-%02d-%02d.out"]=3;
 $plottitle="'Temperatura'";
