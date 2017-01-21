559be8ae5ce9cc14479e58d144d7600c85d10838
venturia <andrea.venturi@cern.ch>
2017-01-21 10:41:26 +0100

    Added configuration for raspberrypi05

---
+++ webpages/index_rpi05.php
@@ -0,0 +1,42 @@
+<?php
+include 'chart_functions.php';
+?>
+
+<html>
+  <meta name = "viewport" content = "width = device-width">
+  <head>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+
+//         document.write("Hello");
+
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+//      google.setOnLoadCallback(drawChart);
+
+<?php
+$phpfilename="index.php";
+$plotwidth=700;
+$plotheight=400;
+?>
+
+<?php
+require('temperature_params.php');
+load_chart_data_from_file($suffix,$logfiletemplates,$dateindex,$timeindex,$addressindex,$tempindex,$offset,$addresses,$labels,$plottitle,$plotwidth,$plotheight,$npastdays[$suffix]);
+?>
+
+    </script>
+  </head>
+
+  <body>
+
+    <form action="<?php echo $phpfilename?>" method="get">
+
+    <?php print_chart_div("temperature",$plotwidth,$plotheight,$npastdays['temperature']); ?>
+   </form>
+  </body>
+</html>
+
---
+++ webpages/temperature_rpi05_params.php
@@ -0,0 +1,18 @@
+<?php
+$suffix="temperature";
+$logfiletemplates=["/home/pi/log_termometro_%04d-%02d-%02d.out"];
+$dateindex=array();
+$dateindex["/home/pi/log_termometro_%04d-%02d-%02d.out"]=0;
+$timeindex=array();
+$timeindex["/home/pi/log_termometro_%04d-%02d-%02d.out"]=1;
+$addressindex=array();
+$addressindex["/home/pi/log_termometro_%04d-%02d-%02d.out"]=6;
+$offset=array();
+$offset["0x4d"]=0;
+$addresses=["0x4d"];
+$labels=["Test DS1621 (C)"];
+$tempindex=array();
+$tempindex["/home/pi/log_termometro_%04d-%02d-%02d.out"]=11;
+$plottitle="'Temperatura'";
+?>
+
