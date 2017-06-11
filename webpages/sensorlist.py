65c0960d53c1bada60471090be28c987f12d778e
Andrea Venturi <andrea.venturi@cern.ch>
2017-06-11 13:52:30 +0200

    Configurazione grafici di default in modulo python separato

--- webpages/sensorlist.py
+++ webpages/sensorlist.py
@@ -3,6 +3,7 @@
 import cgi
 import cgitb
 import StringIO
+import sensorlistdata
 
 cgitb.enable()
 form=cgi.FieldStorage()
@@ -12,17 +13,16 @@ id = form.getvalue("id")
 
 print "Content-type: text/html\n\n"
 
-sensornamelist=["DS1621 sgabuzzino","Temperatura sgabuzzino","Corrente"]
-valuelist=["logtemplate=log_termometro_&sensor=0x4d&label=DS1621 sgabuzzino&unit=C&offset=3","logtemplate=log_DS18b20_&sensor=28-000005958097&label=temperatura sgabuzzino&unit=C&offset=1","logtemplate=log_sct13_&sensor=SCT13_curreff&label=Corrente&unit=A&offset=1"]
-checkedlist=["","",""]
-if id == "0":
-  checkedlist[0]="checked"
-  checkedlist[1]="checked"
-else:
-  checkedlist[2]="checked"
+checkedlist=[]
+for i in range(len(sensorlistdata.checkedbitlist)):
+   checkedlist.append("")
+   if 2**int(id) & sensorlistdata.checkedbitlist[i] == 2**int(id):
+      checkedlist[i]="checked"
+   else:
+      checkedlist[i]=""
 
-for i in range(len(sensornamelist)):
-   tmpstring= "<input type='checkbox' id='sensor%d' "+checkedlist[i]+" value='"+valuelist[i]+"'>"+sensornamelist[i]+"</input>"
+for i in range(len(sensorlistdata.sensornamelist)):
+   tmpstring= "<input type='checkbox' id='sensor%d' "+checkedlist[i]+" value='"+sensorlistdata.valuelist[i]+"'>"+sensorlistdata.sensornamelist[i]+"</input>"
    print >>sensorliststring, tmpstring % i
 
 print sensorliststring.getvalue()
---
+++ webpages/sensorlistdata_rpi05.py
@@ -0,0 +1,5 @@
+sensornamelist=["DS1621 sgabuzzino","Temperatura sgabuzzino","Corrente"]
+valuelist=["logtemplate=log_termometro_&sensor=0x4d&label=DS1621 sgabuzzino&unit=C&offset=3","logtemplate=log_DS18b20_&sensor=28-000005958097&label=temperatura sgabuzzino&unit=C&offset=1","logtemplate=log_sct13_&sensor=SCT13_curreff&label=Corrente&unit=A&offset=1"]
+checkedbitlist=[1,1,3]
+
+
