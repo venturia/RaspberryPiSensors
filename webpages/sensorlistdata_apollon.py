47a079fc16979acffbc3012df119939e4543806b
Andrea Venturi <andrea.venturi@cern.ch>
2017-06-11 14:33:34 +0200

    Parametri per rpi02

---
+++ webpages/sensorlistdata_apollon.py
@@ -0,0 +1,6 @@
+nareas = 2
+sensornamelist=["Casa","Test Piano camere","Sottoscala","Casa"]
+valuelist=["logtemplate=log_bmp085_&sensor=BMP085_temp&label=Casa&unit=C&offset=1","logtemplate=log_DS18b20_&sensor=28-00000596c262&label=Test piano camere&unit=C&offset=1","logtemplate=log_DS18b20_&sensor=28-00000596d046&label=Sottoscala&unit=C&offset=1","logtemplate=log_bmp085_&sensor=BMP085_press&label=Casa&unit=hPa&offset=1"]
+checkedbitlist=[1,1,1,2]
+
+
