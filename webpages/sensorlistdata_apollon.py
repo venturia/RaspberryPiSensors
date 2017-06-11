c2397ad50c737d0f2a96d32d301bea6508316d35
Andrea Venturi <andrea.venturi@cern.ch>
2017-06-11 21:57:34 +0200

    Aggiunti sensori alla configurazione di rpi02

--- webpages/sensorlistdata_apollon.py
+++ webpages/sensorlistdata_apollon.py
@@ -1,6 +1,14 @@
-nareas = 2
-sensornamelist=["Casa","Test Piano camere","Sottoscala","Casa"]
-valuelist=["logtemplate=log_bmp085_&sensor=BMP085_temp&label=Casa&unit=C&offset=1","logtemplate=log_DS18b20_&sensor=28-00000596c262&label=Test piano camere&unit=C&offset=1","logtemplate=log_DS18b20_&sensor=28-00000596d046&label=Sottoscala&unit=C&offset=1","logtemplate=log_bmp085_&sensor=BMP085_press&label=Casa&unit=hPa&offset=1"]
-checkedbitlist=[1,1,1,2]
+nareas = 4
+sensornamelist=["Temperatura casa","Temperatura piano camere","Temperatura sottoscala",
+                "Pressione casa",
+                "Velocita ADSL",
+                "Temperatura CPU"]
+valuelist=["logtemplate=log_bmp085_&sensor=BMP085_temp&label=casa&unit=C&offset=1",
+           "logtemplate=log_DS18b20_&sensor=28-00000596c262&label=Piano camere&unit=C&offset=1",
+           "logtemplate=log_DS18b20_&sensor=28-00000596d046&label=Sottoscala&unit=C&offset=1",
+           "logtemplate=log_bmp085_&sensor=BMP085_press&label=Casa&unit=hPa&offset=1",
+           "logtemplate=log_speedtest_&sensor=Download:&label=Velocita ADSL&unit=Mbps&offset=1",
+           "logtemplate=log_cputemperature_&sensor=cpu&label=Temperatura CPU&unit=C&offset=1"]
+checkedbitlist=[1,1,1,2,4,8]
 
 
