7e2c889defaa5e862e1a086664b661cde6c04acb
Andrea Venturi <andrea.venturi@cern.ch>
2017-06-13 17:06:05 +0200

    Aggiunta configurazione sensori rpi03

---
+++ webpages/sensorlistdata_rpi03.py
@@ -0,0 +1,6 @@
+nareas = 2
+sensornamelist=["Temperatura sala","Velocita ADSL"]
+valuelist=["logtemplate=log_termometro_&sensor=0x4b&label=Temperatura sala&unit=C&offset=3","logtemplate=log_speedtest_&sensor=Download:&label=Velocita ADSL&unit=Mbps&offset=1"]
+checkedbitlist=[1,2]
+
+
