e2c6c2fde98012c657548fbc3bb4ad1aba0554fd
Andrea Venturi <andrea.venturi@cern.ch>
2017-11-02 21:39:07 +0100

    aggiunta lista sensori rpi04

---
+++ webpages/sensorlistdata_rpi04.py
@@ -0,0 +1,10 @@
+nareas = 2
+sensornamelist=["Ingresso","DS18B20 test 1","DS18B20 test 2","DS18B20 sigillato","Corrente"]
+valuelist=["logtemplate=log_DS18b20_&sensor=28-000005959090&label=Ingresso&unit=C&offset=1",
+           "logtemplate=log_DS18b20_&sensor=28-0000059553da&label=DS18B20 test 1&unit=C&offset=1",
+           "logtemplate=log_DS18b20_&sensor=28-00000595887c&label=DS18B20 test 2&unit=C&offset=1",
+           "logtemplate=log_DS18b20_&sensor=28-80000003a017&label=DS18B20 sigillato&unit=C&offset=1",
+"logtemplate=log_sct13_&sensor=SCT13_curreff&label=Corrente&unit=A&offset=1"]
+checkedbitlist=[1,1,1,0,2]
+
+
