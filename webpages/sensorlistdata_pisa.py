d7d6579c14ddd49bb02ca716500cf853df0a7025
Andrea Venturi <andrea.venturi@cern.ch>
2018-09-23 11:51:05 +0200

    Nuova configurazione per rpi06 a Pisa

---
+++ webpages/sensorlistdata_pisa.py
@@ -0,0 +1,8 @@
+nareas = 3
+sensornamelist=["Libreria","Umidita","Dew point"]
+valuelist=["logtemplate=log_sht15_&sensor=SHT15_temp&label=Libreria&unit=C&offset=1",
+           "logtemplate=log_sht15_&sensor=SHT15_rh&label=Umidita relativa&unit=percento&offset=1",
+           "logtemplate=log_sht15_&sensor=SHT15_dp&label=Dew point&unit=C&offset=1"]
+checkedbitlist=[1,2,5]
+
+
