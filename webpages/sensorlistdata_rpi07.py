f7c3daf2ba7fd9d46ebcdcf2db5915278b33fbc8
Andrea Venturi <andrea.venturi@cern.ch>
2018-12-08 10:56:53 +0100

    Aggiunta lista sensori rpi07

---
+++ webpages/sensorlistdata_rpi07.py
@@ -0,0 +1,9 @@
+nareas = 2
+sensornamelist=["Temperatura freezer","Temperatura frigo","Temperatura frigo","Temperatura freezer"]
+valuelist=["logtemplate=log_DS18b20_&sensor=28-0000059659bc&label=temperatura freezer&unit=C&offset=1",
+           "logtemplate=log_DS18b20_&sensor=28-000005956292&label=temperatura frigo&unit=C&offset=1",
+           "logtemplate=log_DS18b20_&sensor=28-0000059553da&label=temperatura frigo&unit=C&offset=1",
+           "logtemplate=log_DS18b20_&sensor=28-00000595887c&label=temperatura freezer&unit=C&offset=1"]
+checkedbitlist=[1,1,2,2]
+
+
